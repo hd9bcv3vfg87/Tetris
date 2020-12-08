f61545284c602395f5c6e1b7ed37333c1e7076e5
hd9bcv3vfg87 <einfall@i.ua>
2020-12-09 00:41:52 +0200

    Dick on screen center added

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,7 +6,35 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            int x1 = 19;
+            int y1 = 15;
+            char c1 = 'X';
+
+            Draw(x1, y1, c1);
+            
+            int x2 = 20;
+            int y2 = 15;
+            char c2 = 'Y';
+
+            Draw(x2, y2, c2);
+            
+            int x3 = 21;
+            int y3 = 15;
+            char c3 = 'Й';
+
+            Draw(x3, y3, c3);
+
+            Console.ReadLine();
         }
+
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+
     }
 }
